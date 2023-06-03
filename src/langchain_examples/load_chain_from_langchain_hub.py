d0f2ab4e22d5b1f4ce72816eac55c1945af116a5
Prasanth Balaraman <prsnth33@gmail.com>
2023-06-03 15:24:51 +0100

    update

---
+++ src/langchain_examples/load_chain_from_langchain_hub.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from langchain.chains import load_chain
+
+load_dotenv()
+
+if __name__ == '__main__':
+    llm_math_chain = load_chain('lc://chains/llm-math/chain.json')
+    print(llm_math_chain.run('What is 42 raised to the power 0.4673?'))
