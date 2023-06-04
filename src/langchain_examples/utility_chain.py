d2f7f5b627864197b4c7af09a1b000176b2bb104
Prasanth Balaraman <prsnth33@gmail.com>
2023-06-04 13:39:46 +0100

    formatted using black

--- src/langchain_examples/utility_chain.py
+++ src/langchain_examples/utility_chain.py
@@ -2,12 +2,14 @@ from dotenv import load_dotenv
 from langchain.chains import LLMMathChain
 from langchain.llms import OpenAI
 
-from src.utils.openapi import count_tokens
+from utils.openapi import count_tokens
 
 load_dotenv()
 
-if __name__ == '__main__':
-    llm = OpenAI(model_name='text-davinci-003')
+if __name__ == "__main__":
+    llm = OpenAI(model_name="text-davinci-003")
     llm_chain = LLMMathChain.from_llm(llm, verbose=True)
-    result = count_tokens(chain=llm_chain, query='What is 13 raised to the .3432 power?')
+    result = count_tokens(
+        chain=llm_chain, query="What is 13 raised to the .3432 power?"
+    )
     print(result)
